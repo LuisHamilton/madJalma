411bc17b41ff825e4d69f29bfd7d6b509203a2a3
LuisHamilton <lhbalem@gmail.com>
2022-05-31 10:51:13 -0300

    moeda 100% funcional

--- MazeJalma/MazeJalma/Form1.cs
+++ MazeJalma/MazeJalma/Form1.cs
@@ -31,10 +31,14 @@ namespace MazeJalma
         int col = 0;
         int coinX;
         int coinY;
+
+        bool spawn = false;
         Random randX = new Random();
         Random randY = new Random();
+        Rectangle soldierRect;
 
         Player playerEvents;
+        Coin coinEvents;
 
         public Form1()
         {
@@ -44,8 +48,8 @@ namespace MazeJalma
             int y = 0;
 
             
-            coinX = randX.Next(500, 3660);
-            coinY = randY.Next(500, 2247);
+            coinX = randX.Next(500, 2660);
+            coinY = randY.Next(500, 1620);
 
             Timer tm = new Timer();
             tm.Interval = 20;
@@ -57,6 +61,7 @@ namespace MazeJalma
                 pictureBox2.SendToBack();
                 g2 = Graphics.FromImage(pictureBox2.Image);
                 playerEvents = new Player(g2);
+                coinEvents = new Coin(g2);
                 tm.Start();
             };
             this.KeyDown += (s, e) =>
@@ -115,25 +120,53 @@ namespace MazeJalma
                 y += speedy;
                 coinX += speedcX;
                 coinY += speedcY;
+
                 if (x < 0)
+                {
                     x = 0;
+                    coinX -= speedcX;
+                }
                 if (y < 0)
+                {
                     y = 0;
+                    coinY -= speedcY;
+                }
+                if (x >= 2660)
+                {
+                    x = 2660;
+                    coinX -= speedcX;
+                }
+                if (y >= 1620)
+                {
+                    y = 1620;
+                    coinY -= speedcY;
+                }
+
                 score.Text = $"{pontos+=col}";
                 
                 playerEvents.mapMove(x, y, mapimage, pictureBox2);
-                playerEvents.coinMove(coinimage, coinX, coinY);
-                playerEvents.rotateSoldier(angle, soldierimage, pictureBox2);
-                col = playerEvents.collisionCoinY();
+                coinEvents.coinMove(coinimage, coinX, coinY);
+                soldierRect = playerEvents.rotateSoldier(angle, soldierimage, pictureBox2);
+                col = coinEvents.collisionCoinY(soldierRect);
 
                 if(col == 1)
                 {
-                    coinX = randX.Next(500, 3660);
-                    coinY = randY.Next(500, 2247);
+                    if (spawn == false)
+                    {
+                        coinX = randX.Next(1660 - x, 2660 - x);
+                        coinY = randY.Next(620 - y, 1620 - y);
+                        spawn = true;
+                    }
+                    else
+                    {
+                        coinX = randX.Next(0 - x + 500, 1660 - x);
+                        coinY = randY.Next(0 - y + 500, 620 - y);
+                        spawn = false;
+                    }
                 }
 
-                lblAmmo.Text = $"{x} - {y}";
-                lblKill.Text = $"{coinX} - {coinY}";
+                lblAmmo.Text = $"{x}-{y}";
+                lblKill.Text = $"{coinX}-{coinY}";
 
                 pictureBox2.Refresh();
             };
--- MazeJalma/MazeJalma/Player.cs
+++ MazeJalma/MazeJalma/Player.cs
@@ -30,46 +30,8 @@ namespace MazeJalma
                 new Rectangle(0, 0, pb.Width, pb.Height),
                 new Rectangle(x, y, pb.Width, pb.Height), GraphicsUnit.Pixel);
         }
-        public void coinMove(Bitmap coin, int coinX, int coinY)
-        {
-            int coinW = 50;
-            int coinH = 50;
-            g.TranslateTransform(coinX, coinY);
-            g.DrawImage(coin,
-                new Rectangle(0, 0, coinW, coinH),
-                new Rectangle(0, 0, coinW * 4, coinH * 4), GraphicsUnit.Pixel);
-            g.TranslateTransform(-coinX, -coinY);
-
-            coinRect = new Rectangle(coinX, coinY, coinW, coinH);
-        }
-        public int collisionCoinY()
-        {
-            g.DrawRectangle(Pens.Red, coinRect);
-            g.DrawRectangle(Pens.Red, soldierRect);
-
-            if (soldierRect.Top <= coinRect.Bottom && soldierRect.Top > coinRect.Top && coinRect.Left >= soldierRect.Left && coinRect.Left <= soldierRect.Right ||
-                soldierRect.Top <= coinRect.Bottom && soldierRect.Top > coinRect.Top && coinRect.Right >= soldierRect.Left && coinRect.Right <= soldierRect.Right)
-            {
-                return 1;
-            }
-            if(soldierRect.Bottom >= coinRect.Top && soldierRect.Bottom < coinRect.Bottom && coinRect.Left >= soldierRect.Left && coinRect.Left <= soldierRect.Right ||
-               soldierRect.Bottom >= coinRect.Top && soldierRect.Bottom < coinRect.Bottom && coinRect.Right >= soldierRect.Left && coinRect.Right <= soldierRect.Right)
-            {
-                return 1;
-            }
-            if (soldierRect.Right >= coinRect.Left && soldierRect.Right < coinRect.Right && coinRect.Top >= soldierRect.Top && coinRect.Top <= soldierRect.Bottom ||
-               soldierRect.Right >= coinRect.Left && soldierRect.Right < coinRect.Right && coinRect.Bottom >= soldierRect.Top && coinRect.Bottom <= soldierRect.Bottom)
-            {
-                return 1;
-            }
-            if (soldierRect.Left <= coinRect.Right && soldierRect.Left > coinRect.Left && coinRect.Top >= soldierRect.Top && coinRect.Top <= soldierRect.Bottom ||
-               soldierRect.Left <= coinRect.Right && soldierRect.Left > coinRect.Left && coinRect.Bottom >= soldierRect.Top && coinRect.Bottom <= soldierRect.Bottom)
-            {
-                return 1;
-            }
-            return 0;
-        }
-        public void rotateSoldier(float angle, Bitmap btm, PictureBox pb)
+        
+        public Rectangle rotateSoldier(float angle, Bitmap btm, PictureBox pb)
         {
 
             int deslocx = pb.Width / 2;
@@ -87,6 +49,8 @@ namespace MazeJalma
 
             g.RotateTransform(-angle);
             g.TranslateTransform(-deslocx, -deslocy);
+
+            return soldierRect;
         }
     }
 }
