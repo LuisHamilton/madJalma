cf6de2ca08d65d052d25e174e5173f1d0a752066
LuisHamilton <lhbalem@gmail.com>
2022-05-31 11:10:03 -0300

    começando a criar bullet

--- MazeJalma/MazeJalma/Bullet.cs
+++ MazeJalma/MazeJalma/Bullet.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+
 
 namespace MazeJalma
 {
-    internal class Bullet
+    public class Bullet
     {
+        Graphics g;
+        float angle;
+
+        public Bullet (Graphics g)
+        {
+            this.g = g;
+            this.angle = angle;
+        }
+
+        public void Loop(float angle)
+        {
+
+        }
     }
 }
